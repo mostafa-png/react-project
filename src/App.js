d28c642806a58a026cbfa81f9e3c84ae93ce8a9a
mostafa-png <mo3563935@gmail.com>
2024-06-08 23:46:56 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2 className="title">c4a</h2>
+        <h3>Mostafa khaled</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
